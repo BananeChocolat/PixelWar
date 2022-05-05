97a87bf406aceb6b9d3bf42042fcfb33577624ff
BananeChocolat <jy.aurelien@gmail.com>
2022-05-05 20:48:17 +0200

    debut clean css

--- frontend/script.js
+++ frontend/script.js
@@ -48,50 +48,6 @@ ctx.putImageData(imageData, 0, 0);
 
 
 window.onload = function(){ 
-<<<<<<< Updated upstream
-  var scale = 1,
-    panning = false,
-    pointX = 0,
-    pointY = 0,
-    start = { x: 0, y: 0 };
-
-    var zoom = document.getElementById('zoom');
-
-    function setTransform() {
-      zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
-    }
-    
-    zoom.onmousedown = function (e) {
-      e.preventDefault();
-      start = { x: e.clientX - pointX, y: e.clientY - pointY };
-      panning = true;
-    }
-    
-    zoom.onmouseup = function (e) {
-      panning = false;
-    }
-    
-    zoom.onmousemove = function (e) {
-      e.preventDefault();
-      if (!panning) {
-        return;
-      }
-      pointX = (e.clientX - start.x);
-      pointY = (e.clientY - start.y);
-      setTransform();
-    }
-    
-    zoom.onwheel = function (e) {
-      e.preventDefault();
-      var xs = (e.clientX - pointX) / scale,
-        ys = (e.clientY - pointY) / scale,
-        delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
-      (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
-      pointX = e.clientX - xs * scale;
-      pointY = e.clientY - ys * scale;
-      setTransform();
-    }
-=======
 	var scale = 1,
 	panning = false,
 	pointX = 0,
@@ -155,6 +111,5 @@ window.onload = function(){
 			
 		};
 	}
->>>>>>> Stashed changes
 };
 
